7b9d121cbbf264c9b6a76bc569884b6196a0f62f
Mohammad <moh.osama84x@gmail.com>
2021-09-12 21:08:07 +0200

    Creating navigation bar

--- js/app.js
+++ js/app.js
@@ -1,57 +1,17 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
+ // Define Global Variables 
 
-/**
- * Define Global Variables
- * 
-*/
+const sections = document.querySelectorAll('section');
+const fragment = document.createDocumentFragment();
 
+// creating list items 
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+sections.forEach((section) => {
+    let listItem  = document.createElement('li') ;
+    let link = section.getAttribute("id");
+    let text = section.getAttribute("data-nav");
+    listItem.innerHTML = `<a href="#${link}">${text}</a>`;
+    fragment.appendChild(listItem);
+      });
 
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
+document.getElementById('navbar__list').appendChild(fragment) ;
 
