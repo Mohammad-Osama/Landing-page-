be943c767571ea8b75fea7f35d2f499fdc6a1b4b
Mohammad <moh.osama84x@gmail.com>
2021-09-12 22:46:50 +0200

    Adding Event Listener

--- js/app.js
+++ js/app.js
@@ -9,9 +9,16 @@ sections.forEach((section) => {
     let listItem  = document.createElement('li') ;
     let link = section.getAttribute("id");
     let text = section.getAttribute("data-nav");
-    listItem.innerHTML = `<a href="#${link}">${text}</a>`;
-    fragment.appendChild(listItem);
-      });
+    listItem.innerHTML = `<a href="#${link}">${text}</a>`; 
 
+    // Event listener with a scroll
+    listItem.addEventListener('click',(x)=> {
+        x.preventDefault();
+        document.getElementById(link).scrollIntoView({behavior: "smooth"});
+              })
+    fragment.appendChild(listItem);
+        });
+        
+// appending all changes to the document 
 document.getElementById('navbar__list').appendChild(fragment) ;
 
